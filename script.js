f6ea7a48a70a7d120eb20fa375ff232360391065
Kaela Han <kaehan@ucdavis.edu>
2018-03-27 11:59:24 -0700

    Audio Control Buttons
    Style buttons.

--- script.js
+++ script.js
@@ -161,6 +161,7 @@ radial.addEventListener('click', function() {
 function showBarClick() {
   fill(currentColor);
   noStroke();
+  translate(-25,0);
   for (var i = 0; i < spectrum.length; i++) {
     var x = map(i, 0, spectrum.length, 0, width);
     var h = -height + map(spectrum[i], 0, 255, height, 0);
